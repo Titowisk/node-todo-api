f332dc2e5607e67f55ebe08e6688a6f5939f91b0
Vitor Rabelo <rabelo51@gmail.com>
2018-04-03 15:33:14 -0300

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,9 +6,20 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [
+    {
+        text: "First test todo"
+    },
+    {
+        text: "Second test todo"
+    }
+];
+
 // Sets the database before each test
 beforeEach((done) => {
-    Todo.remove({}).then(() => done()); // removes all data from Todo collection before each test
+    Todo.remove({}).then(() => { // removes all data from Todo collection before each test
+        return Todo.insertMany(todos); // using return allow to chain callbacks
+    }).then(() => done()); 
 });
 
 describe('POST /todos', () => {
@@ -28,7 +39,7 @@ describe('POST /todos', () => {
                     return done(err); // return to stop the function exectuion in case of an error
                 }
                 // tests the database
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -48,9 +59,21 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
     })
 });
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done); // no need to use a callback because there is nothing asynchronous here
+    })
+})
